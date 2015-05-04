31fc1152d99c193b6c39c61dcf3f4f6925954748
countless-integers <totcha@gazeta.pl>
2015-05-04 10:27:27 +0200

    fix: missing configs in setup script

--- setItUp.sh
+++ setItUp.sh
@@ -16,10 +16,12 @@ do
     echo -e "\n"
 done <<DOTFILE_TO_LINK_DESTINATION_MAPPING
 $HOME/dotfiles/bash/bash_profile    $HOME/.bash_profile 
+$HOME/dotfiles/bash/bashrc          $HOME/.bashrc 
 $HOME/dotfiles/git/gitconfig        $HOME/.gitconfig 
 $HOME/dotfiles/git/gitignore        $HOME/.gitignore 
 $HOME/dotfiles/mc                   $HOME/.config/mc 
 $HOME/dotfiles/screen/screenrc      $HOME/.screenrc
+$HOME/dotfiles/tmux/tmux.conf       $HOME/.tmux.conf
 $HOME/dotfiles/vim/vimrc            $HOME/.vimrc
 DOTFILE_TO_LINK_DESTINATION_MAPPING
 
