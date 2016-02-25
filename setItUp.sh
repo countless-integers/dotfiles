7e82efc8775ad95e3597daafe98e1015d913f764
countless-integers <totcha@gazeta.pl>
2016-02-25 13:45:03 +0100

    eslint support
    git config pruning

--- setItUp.sh
+++ setItUp.sh
@@ -5,7 +5,7 @@ echo -e Linking config files..."\n"
 backupsuffix="bak$(date +%Y%m%d%H%M)"
 while read -r source target
 do
-    if [ ! -e $source ]; then 
+    if [ ! -e $source ]; then
         echo Trying to link an inexisting dotfile: $source, skipping...
         continue
     fi
@@ -15,15 +15,16 @@ do
     ln -vs $source $target
     echo -e "\n"
 done <<DOTFILE_TO_LINK_DESTINATION_MAPPING
-$HOME/dotfiles/bash/bash_profile    $HOME/.bash_profile 
-$HOME/dotfiles/bash/bashrc          $HOME/.bashrc 
-$HOME/dotfiles/git/gitconfig        $HOME/.gitconfig 
-$HOME/dotfiles/git/gitignore        $HOME/.gitignore 
-$HOME/dotfiles/mc                   $HOME/.config/mc 
-$HOME/dotfiles/mc/skins             $HOME/.local/share/mc/skins 
+$HOME/dotfiles/bash/bash_profile    $HOME/.bash_profile
+$HOME/dotfiles/bash/bashrc          $HOME/.bashrc
+$HOME/dotfiles/git/gitconfig        $HOME/.gitconfig
+$HOME/dotfiles/git/gitignore        $HOME/.gitignore
+$HOME/dotfiles/mc                   $HOME/.config/mc
+$HOME/dotfiles/mc/skins             $HOME/.local/share/mc/skins
 $HOME/dotfiles/screen/screenrc      $HOME/.screenrc
 $HOME/dotfiles/tmux/tmux.conf       $HOME/.tmux.conf
 $HOME/dotfiles/vim/vimrc            $HOME/.vimrc
+$HOME/dotfiles/eslintrc.json        $HOME/.eslintrc.json
 # @todo: deal with dirs
 DOTFILE_TO_LINK_DESTINATION_MAPPING
 
@@ -32,6 +33,7 @@ echo -e ...everything in its place."\n"
 if [ ! -d ~/.vim/bundle/Vundle.vim ]; then
     echo -e installing Vundle for Vim."\n"
     git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+    vim +PluginInstall +qall &>/dev/null
 fi
 
 echo "To find backups use: find $HOME -iname '*.$backupsuffix'"
