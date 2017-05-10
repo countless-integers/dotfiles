455b0c9583949c511095d0e89d11455162d745e4
countless-integers <totcha@gazeta.pl>
2017-05-10 09:34:52 +0200

    change zsh theme
    -D flag to pruneLocalBranches
    
    show invisibles in atom

--- bin/pruneLocalBranches.sh
+++ bin/pruneLocalBranches.sh
@@ -2,6 +2,12 @@
 git fetch -ap &>/dev/null
 git branch -vv | grep ": gone]" | awk '{ print $1 }'
 read -p "Delete those? [y|n]" answer
-if [ $answer == "y" ]; then 
-    git branch -vv | grep ": gone]" | awk '{ print $1 }' | xargs -n 1 git branch -d
+if [ "$answer" == "y" ]; then
+
+    read -p "Use force? [y|n]" answer
+    if [ "$answer" == "y" ]; then
+        git branch -vv | grep ": gone]" | awk '{ print $1 }' | xargs -n 1 git branch -D
+    else
+        git branch -vv | grep ": gone]" | awk '{ print $1 }' | xargs -n 1 git branch -d
+    fi
 fi
