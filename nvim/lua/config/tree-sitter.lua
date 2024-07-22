7e16d77e363dd4d7f64b4cbd10f43a9ba4684971
Adrian <oxygen-nitrogen@outlook.com>
2024-07-22 18:00:58 +0200

    chore(nvim): add vimdoc to treesitter ensure_installed to prevent ts spazing out in vim help files

--- nvim/lua/config/tree-sitter.lua
+++ nvim/lua/config/tree-sitter.lua
@@ -1,8 +1,15 @@
 local configs = require("nvim-treesitter.configs")
 
 configs.setup({
-  ensure_installed = { "lua", "bash", "javascript", "html" },
+  ensure_installed = {
+    "lua",
+    "bash",
+    "javascript",
+    "html",
+    -- this is to stop help pages from crashing https://github.com/neovim/neovim/issues/29492
+    "vimdoc",
+  },
   sync_install = false,
   highlight = { enable = true },
-  indent = { enable = true },  
+  indent = { enable = true },
 })
