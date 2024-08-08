1166fa10f2c638adbb014f76d16ba4f619c63fe0
Adrian <oxygen-nitrogen@outlook.com>
2024-08-08 16:44:37 +0200

    chore(nvim): share clipboard in nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,6 +3,7 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
+vim.o.clipboard = "unnamedplus"
 
 -- Plugins configs
 require('config.lazy')
