443951ff4f53b30fe380ee2d6c9db9b2e0fc7932
Adrian <oxygen-nitrogen@outlook.com>
2024-08-11 13:06:02 +0200

    chore(nvim): esc mapping for built-in terminal

--- nvim/init.lua
+++ nvim/init.lua
@@ -5,6 +5,9 @@ vim.o.softtabstop = 2
 vim.o.shiftwidth = 2
 vim.o.clipboard = "unnamedplus"
 vim.o.rnu = true
+-- exit terminal without closing, 
+-- similar to how going back from insert to normal mode in a buffer works
+vim.keymap.set('t', '<Esc>', [[<C-\><C-n>]])
 
 -- Plugins configs
 require('config.lazy')
