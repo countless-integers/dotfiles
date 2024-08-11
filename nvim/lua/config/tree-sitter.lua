2cf4c7e9b22fe2fd181376dd16674c6f195afd31
Adrian <oxygen-nitrogen@outlook.com>
2024-08-11 12:44:20 +0200

    feat(nvim): try out noice plugin

--- nvim/lua/config/tree-sitter.lua
+++ nvim/lua/config/tree-sitter.lua
@@ -8,6 +8,11 @@ configs.setup({
     "html",
     -- this is to stop help pages from crashing https://github.com/neovim/neovim/issues/29492
     "vimdoc",
+    -- noice recommendations
+    "vim",
+    "regex",
+    "markdown",
+    "markdown_inline",
   },
   sync_install = false,
   highlight = { enable = true },
