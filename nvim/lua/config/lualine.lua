282ebaa40f12f1286a01ccef7e513c6fc6d85f18
Adrian <oxygen-nitrogen@outlook.com>
2024-08-19 17:22:14 +0200

    chore(nvim): try-out some lualine customizations

--- nvim/lua/config/lualine.lua
+++ nvim/lua/config/lualine.lua
@@ -2,4 +2,16 @@ local lualine = require("lualine")
 
 lualine.setup({
   theme = "catppuccin",
+  sections = {
+    lualine_b = {
+      "fancy_branch",
+      "fancy_diff",
+    },
+    lualine_x = {
+      "fancy_lsp_servers",
+      "encoding",
+      "fileformat",
+      "filetype",
+    },
+  },
 })
