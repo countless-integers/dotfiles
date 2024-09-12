26f8d0d06a4f3b520e6d7e91eb10276b59dfa8f2
Adrian <oxygen-nitrogen@outlook.com>
2024-09-12 13:54:02 +0200

    chore(nvim): disable branch name in lualine because it takes a ton of space

--- nvim/lua/config/lualine.lua
+++ nvim/lua/config/lualine.lua
@@ -1,17 +1,17 @@
 local lualine = require("lualine")
 
 lualine.setup({
-  theme = "catppuccin",
-  sections = {
-    lualine_b = {
-      "fancy_branch",
-      "fancy_diff",
-    },
-    lualine_x = {
-      "fancy_lsp_servers",
-      "encoding",
-      "fileformat",
-      "filetype",
-    },
-  },
+	theme = "catppuccin",
+	sections = {
+		lualine_b = {
+			-- 	"fancy_branch",
+			"fancy_diff",
+		},
+		lualine_x = {
+			"fancy_lsp_servers",
+			"encoding",
+			"fileformat",
+			"filetype",
+		},
+	},
 })
