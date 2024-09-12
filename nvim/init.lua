b3ea903ba052a4cb25d0061c2abae3edf323a163
Adrian <oxygen-nitrogen@outlook.com>
2024-09-12 13:54:28 +0200

    chore(nvim): add case settings for search

--- nvim/init.lua
+++ nvim/init.lua
@@ -5,20 +5,22 @@ vim.o.softtabstop = 2
 vim.o.shiftwidth = 2
 vim.o.clipboard = "unnamedplus"
 vim.o.rnu = true
--- exit terminal without closing, 
+-- exit terminal without closing,
 -- similar to how going back from insert to normal mode in a buffer works
-vim.keymap.set('t', '<Esc>', [[<C-\><C-n>]])
+vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]])
+vim.o.ignorecase = true
+vim.o.smartcase = true
 
 -- Plugins configs
-require('config.lazy')
-require('config.catppuccin')
-require('config.telescope')
-require('config.file-navigation')
-require('config.tree-sitter')
-require('config.lualine')
-require('config.lsp')
-require('config.alpha')
-require('config.completions')
-require('config.debugger')
-require('config.git-integrations')
-require('config.noice')
+require("config.lazy")
+require("config.catppuccin")
+require("config.telescope")
+require("config.file-navigation")
+require("config.tree-sitter")
+require("config.lualine")
+require("config.lsp")
+require("config.alpha")
+require("config.completions")
+require("config.debugger")
+require("config.git-integrations")
+require("config.noice")
