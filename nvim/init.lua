c80cace22de78683c37224dce848fe72da21ca2d
Adrian <oxygen-nitrogen@outlook.com>
2024-09-24 21:12:14 +0200

    chore(nvim): unimpaired-like mappings and search settings

--- nvim/init.lua
+++ nvim/init.lua
@@ -5,12 +5,23 @@ vim.o.softtabstop = 2
 vim.o.shiftwidth = 2
 vim.o.clipboard = "unnamedplus"
 vim.o.rnu = true
--- exit terminal without closing,
--- similar to how going back from insert to normal mode in a buffer works
--- also see autocommands for starting insert mode when terminal is opened 
-vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]])
 vim.o.ignorecase = true
 vim.o.smartcase = true
+
+local unimpared_mappings = {
+	b = { ":bprevious<CR>", ":bnext<CR>" },
+	w = { "<C-w>p", "<C-w>w" },
+	q = { ":cprevious<CR>", ":cnext<CR>" },
+	l = { ":lprevious<CR>", ":lnext<CR>" },
+}
+for key, cmds in pairs(unimpared_mappings) do
+	vim.keymap.set("n", "[" .. key, cmds[1], { silent = true })
+	vim.keymap.set("n", "]" .. key, cmds[2], { silent = true })
+end
+-- exit terminal without closing,
+-- similar to how going back from insert to normal mode in a buffer works
+-- also see autocommands for starting insert mode when terminal is opened
+vim.keymap.set("t", "<Esc>", "<C-\\><C-n>", { silent = true })
 
 -- Plugins configs
 require("config.lazy")
