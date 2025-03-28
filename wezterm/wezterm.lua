692675d7c4d6e02c50698d2bd767678cc781a76e
Adrian <oxygen-nitrogen@outlook.com>
2025-03-28 10:40:35 +0100

    feat(wezterm): follow system theme (dark/light)

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -3,11 +3,32 @@ local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
 -- Appearance
-config.color_scheme = 'Catppuccin Macchiato'
+
+-- wezterm.gui is not available to the mux server, so take care to
+-- do something reasonable when this config is evaluated by the mux
+function get_appearance()
+  if wezterm.gui then
+    return wezterm.gui.get_appearance()
+  end
+  return 'Dark'
+end
+
+function scheme_for_appearance(appearance)
+  if appearance:find 'Dark' then
+    return 'Catppuccin Macchiato'
+  else
+    -- return 'Catppuccin Latte'
+    return 'Gruvbox Light'
+  end
+end
+
+config.color_scheme = scheme_for_appearance(get_appearance())
 config.hide_tab_bar_if_only_one_tab = true
 config.font = wezterm.font "JetBrainsMono Nerd Font Mono"
 config.font_size = 16
 config.line_height = 1.2
+
+config.command_palette_font_size = config.font_size
 
 config.window_decorations = "RESIZE"
 config.enable_tab_bar = false
