6c10266216adbfeb12ec79b0396c3308c64eb6f5
countless-integers <oxygen-nitrogen@outlook.com>
2025-06-12 22:56:10 +0200

    fix(wezterm): line_height 1.2 doesn't look good on wayland for reasons

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -23,9 +23,12 @@ end
 
 config.color_scheme = scheme_for_appearance(get_appearance())
 config.hide_tab_bar_if_only_one_tab = true
-config.font = wezterm.font "JetBrainsMono Nerd Font Mono"
+config.font = wezterm.font_with_fallback {
+  "JetBrainsMono Nerd Font Mono",
+  "JetBrains Mono",
+}
 config.font_size = 16
-config.line_height = 1.2
+config.line_height = 1.3
 
 config.command_palette_font_size = config.font_size
 
