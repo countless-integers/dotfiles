907756bd6a3f85786acbf71c1ca86b88e63bc404
Adrian <oxygen-nitrogen@outlook.com>
2025-07-22 10:29:11 +0200

    chore(nvim): end of another era, remove neotree because of snacks

--- nvim/lua/config/file-navigation.lua
+++ nvim/lua/config/file-navigation.lua
@@ -13,21 +13,3 @@ require("oil").setup({
 
 vim.keymap.set('n', '<leader>-', ":Oil<CR>")
 
-require("neo-tree").setup({
-  close_if_last_window = true,
-  sources = {
-    'filesystem',
-    'buffers',
-    'document_symbols',
-  },
-  window = {
-    mappings = {
-      ["<space>"] = { "toggle_node", nowait = true },
-    }
-  }
-})
-
-vim.keymap.set("n", "<leader>n", ":Neotree filesystem toggle left reveal_force_cwd<CR>")
-vim.keymap.set("n", "<leader>N", ":Neotree filesystem toggle float reveal_force_cwd<CR>")
-vim.keymap.set("n", "<leader>s", ":Neotree document_symbols toggle float reveal<CR>")
-vim.keymap.set("n", "<leader>b", ":Neotree buffers toggle float reveal<CR>")
--- nvim/lua/plugins/file-navigation.lua
+++ nvim/lua/plugins/file-navigation.lua
@@ -1,15 +1,5 @@
 return {
   {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons",
-      "MunifTanjim/nui.nvim",
-    },
-    lazy = false,
-  },
-  {
     "stevearc/oil.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" },
   },
