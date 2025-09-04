a79b5d6e63cabb0376bf9aa3beabc53d6a13a066
Adrian <oxygen-nitrogen@outlook.com>
2025-09-04 15:26:57 +0200

    chore(nvim): prevent padding brackets with space in mini.surround

--- nvim/lua/config/mini.lua
+++ nvim/lua/config/mini.lua
@@ -1,4 +1,11 @@
 require('mini.ai').setup()
-require('mini.surround').setup()
+require('mini.surround').setup({
+  custom_surroundings = {
+    ['('] = { output = { left = '(', right = ')' } },
+    ['['] = { output = { left = '[', right = ']' } },
+    ['{'] = { output = { left = '{', right = '}' } },
+    ['<'] = { output = { left = '<', right = '>' } },
+  },
+})
 require('mini.pairs').setup()
 require('mini.bracketed').setup()
