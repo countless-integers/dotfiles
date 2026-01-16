ebbb7219870f58f9c1f862d316b98a149a4fddda
countless-integers <oxygen-nitrogen@outlook.com>
2026-01-16 18:37:04 +0100

    refactor(nvim): treesitter config migration

--- nvim/lua/config/tree-sitter.lua
+++ nvim/lua/config/tree-sitter.lua
@@ -1,39 +1,44 @@
 ---@diagnostic disable: missing-fields
-require("nvim-treesitter.configs").setup({
-  ensure_installed = {
-    "lua",
-    "bash",
-    -- this is to stop help pages from crashing https://github.com/neovim/neovim/issues/29492
-    "vimdoc",
-    "vim",
-    "regex",
-    "markdown",
-    "markdown_inline",
-    -- data 
-    "yaml",
-    "json",
-    -- other
-    "javascript",
-    "html",
-    "css",
-    "vue",
-    "terraform",
-    "hcl",
-    "nu",
-    "rust",
-    "php",
-  },
-  auto_install = true,
-  sync_install = false,
-  highlight = { enable = true },
-  indent = { enable = true },
-  -- disable for large files
-  disable = function(lang, buf)
-    local max_filesize = 100 * 1024 -- 100KB
-    local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-    if ok and stats and stats.size > max_filesize then
-      return true
-    end
-  end,
-  additional_vim_regex_highlighting = false,
-})
+local treesitter = require("nvim-treesitter")
+
+local languages = {
+  "lua",
+  "bash",
+  -- this is to stop help pages from crashing https://github.com/neovim/neovim/issues/29492
+  "vimdoc",
+  "vim",
+  "regex",
+  "markdown",
+  "markdown_inline",
+  -- data 
+  "yaml",
+  "json",
+  -- other
+  "javascript",
+  "html",
+  "css",
+  "vue",
+  "terraform",
+  "hcl",
+  "nu",
+  "rust",
+  "php",
+}
+treesitter.setup()
+treesitter.install(languages)
+
+vim.api.nvim_create_autocmd(
+  'FileType',
+  {
+    pattern = languages,
+    callback = function()
+      -- syntax highlighting, provided by Neovim
+      vim.treesitter.start()
+      -- folds, provided by Neovim 
+      vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+      vim.wo.foldmethod = 'expr'
+      -- indentation, provided by nvim-treesitter
+      vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+    end,
+  }
+)
--- nvim/lua/plugins/tree-sitter.lua
+++ nvim/lua/plugins/tree-sitter.lua
@@ -1,5 +1,6 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  --build means to call this every time the plugin is updated
-  build = ":TSUpdate",
+	"nvim-treesitter/nvim-treesitter",
+	--build means to call this every time the plugin is updated
+	build = ":TSUpdate",
+	lazy = false,
 }
