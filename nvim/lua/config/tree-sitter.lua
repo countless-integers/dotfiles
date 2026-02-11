815b7b70cecd3ed0e5da0f267c4117b1e4c2521d
Adrian <oxygen-nitrogen@outlook.com>
2026-02-11 17:43:02 +0100

    refactor(nvim): experimenting with tree-sitter interjections and more robust parser autocmd

--- nvim/lua/config/tree-sitter.lua
+++ nvim/lua/config/tree-sitter.lua
@@ -2,43 +2,68 @@
 local treesitter = require("nvim-treesitter")
 
 local languages = {
-  "lua",
-  "bash",
-  -- this is to stop help pages from crashing https://github.com/neovim/neovim/issues/29492
-  "vimdoc",
-  "vim",
-  "regex",
-  "markdown",
-  "markdown_inline",
-  -- data 
-  "yaml",
-  "json",
-  -- other
-  "javascript",
-  "html",
-  "css",
-  "vue",
-  "terraform",
-  "hcl",
-  "nu",
-  "rust",
-  "php",
+	"lua",
+	"bash",
+	"nu",
+	"rust",
+	"php",
+	-- this is to stop help pages from crashing https://github.com/neovim/neovim/issues/29492
+	"vimdoc",
+	"vim",
+	"regex",
+	-- templating
+	"markdown",
+	"markdown_inline",
+	"jinja",
+	"jinja_inline",
+	-- data
+	"yaml",
+	"json",
+	-- fe
+	"javascript",
+	"html",
+	"css",
+	"vue",
+	-- iac
+	"terraform",
+	"hcl",
 }
 treesitter.setup()
 treesitter.install(languages)
 
-vim.api.nvim_create_autocmd(
-  'FileType',
-  {
-    pattern = languages,
-    callback = function()
-      -- syntax highlighting, provided by Neovim
-      vim.treesitter.start()
-      -- folds, provided by Neovim 
-      vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
-      vim.wo.foldmethod = 'expr'
-      -- indentation, provided by nvim-treesitter
-      vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
-    end,
-  }
-)
+vim.filetype.add({
+	extension = {
+		j2 = function(path, bufnr)
+			-- Check if there is a second extension (e.g., .sh.j2)
+			local base = path:match("([^/]+)%.j2$")
+			local sub_ext = base:match("%.([^.]+)$")
+
+			if sub_ext then
+				-- jinja2 parser doesn't seem to exist so use jinja
+				return sub_ext .. ".jinja"
+			end
+			return "jinja"
+		end,
+	},
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = languages,
+	callback = function(args)
+		local ft = vim.bo[args.buf].filetype
+
+		-- Check if a treesitter parser is actually available for this filetype
+		local has_parser, _ = pcall(vim.treesitter.get_parser, args.buf)
+
+		if has_parser then
+			vim.treesitter.start()
+			vim.wo.foldmethod = "expr"
+			vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+			-- Ensure nvim-treesitter is loaded before calling its indentexpr
+			vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+		else
+			-- Fallback to standard Vim regex highlighting/indenting
+			vim.bo.indentexpr = ""
+		end
+	end,
+})
