939c14f8a444e25d1fe1823a2413228abf04f877
Adrian <oxygen-nitrogen@outlook.com>
2026-02-16 11:05:05 +0100

    chore(nvim): format file-navigation

--- nvim/lua/config/file-navigation.lua
+++ nvim/lua/config/file-navigation.lua
@@ -1,15 +1,14 @@
 require("oil").setup({
-  default_file_explorer = true,
-  delete_to_trash = true,
-  skip_confirm_for_simple_edits = false,
-  view_options = {
-    show_hidden = false,
-    natural_order = true,
-  },
-  win_options = {
-    wrap = true,
-  },
+	default_file_explorer = true,
+	delete_to_trash = true,
+	skip_confirm_for_simple_edits = false,
+	view_options = {
+		show_hidden = false,
+		natural_order = true,
+	},
+	win_options = {
+		wrap = true,
+	},
 })
 
-vim.keymap.set('n', '<leader>-', ":Oil<CR>")
-
+vim.keymap.set("n", "<leader>-", ":Oil<CR>")
